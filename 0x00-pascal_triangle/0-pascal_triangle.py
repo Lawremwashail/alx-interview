049d396b9180666ac7b7818bab2c481e5ce64db0
Lawremwashail <lawremwash@gmail.com>
2024-10-06 16:13:58 +0300

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@ pascal's triangle
 
 def pascal_triangle(n):
     """
-    checks if n is empty
+    checks if trinagle is empty
     returns an empty list if n is empty
     intitializes empty triangle list
     """
@@ -15,14 +15,14 @@ def pascal_triangle(n):
     if n <= 0:
         return triangle
 
-    triangle.append([1])
+    triangle = [[1]]
 
-    for i in range(n):
+    for i in range(1, n):
         row = [1]
 
         for j in range(len(triangle[i - 1]) - 1):
+            current_row = triangle[i - 1]
             row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         row.append(1)
         triangle.append(row)
-
     return triangle
