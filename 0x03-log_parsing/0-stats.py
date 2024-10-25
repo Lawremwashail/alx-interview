31881d6dda50d5f8b2fec60cbbe33608c6e10fe6
Lawremwashail <lawremwash@gmail.com>
2024-10-25 10:38:23 +0200

    logparsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Log parsing
+"""
+
+
+import sys
+
+total_file_size = 0
+line_count = 0
+
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+status_counts = {code: 0 for code in status_codes}
+
+
+def print_metrics(status: dict, file_size: int) -> None:
+    """
+    Prints the current metrics: total file size and count of each status code.
+    Prints only none zero status code counts.
+
+    Args:
+        status (dict): status_codes
+        file_size (int): total file size
+    """
+    print("File size: {:d}".format(file_size))
+
+    for code, count in sorted(status.items()):
+        if count > 0:
+            print("{}: {}".format(code, count))
+
+
+try:
+    for line in sys.stdin:
+        line_count += 1
+        parts = line.split()
+
+        try:
+            status_codes = parts[-2]
+            if status_codes in status_counts:
+                status_counts[status_codes] += 1
+        except IndexError:
+            pass
+
+        try:
+            file_size = int(parts[-1])
+            total_file_size += file_size
+        except (IndexError, ValueError):
+            pass
+
+        if line_count % 10 == 0:
+            print_metrics(status_counts, total_file_size)
+
+        print_metrics(status_counts, total_file_size)
+
+except KeyboardInterrupt:
+    print_metrics(status_counts, total_file_size)
+    raise
